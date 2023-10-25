540a14c1b2238262f49ef784556426b01c3b921f
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-10-25 19:18:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pam5/Data/DataFrom.kt
@@ -0,0 +1,5 @@
+package com.example.pam5.Data
+
+data class DataFrom(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/pam5/Data/DataSource.kt
@@ -0,0 +1,9 @@
+package com.example.pam5.Data
+import com.example.pam5.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
